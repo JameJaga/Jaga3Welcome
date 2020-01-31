3f7dd8dd3ea0284450fb63211a787bbc82056779
JameJaga <59597475+JameJaga@users.noreply.github.com>
2020-01-31 18:05:00 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
 
+import discord
+import os
+
+TOKEN = os.environ.get("DISCORD_TOKEN")
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):            
+client.run(TOKEN)
