3fc43a711de075fb629e15f3479c4a5a305dbb6f
JameJaga <59597475+JameJaga@users.noreply.github.com>
2020-01-31 18:27:39 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,5 +11,8 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
-async def on_message(message):            
+async def on_member_join(member):
+    channel = client.get_guild(662153006787199046)
+    role = discord.utils.get(message.guild.roles, name='NotCertified')
+    await member.add_roles(role)
 client.run(TOKEN)
