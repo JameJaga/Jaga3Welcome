9aa28e74f711f6db5b968e87bf59e5383ed60c4b
JameJaga <59597475+JameJaga@users.noreply.github.com>
2020-02-03 18:25:09 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ TOKEN = os.environ.get("DISCORD_TOKEN")
 
 client = discord.Client()
 
+guild = client.get_guild(662153006787199046)
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
