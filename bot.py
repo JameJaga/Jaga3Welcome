36785c659863a23e096e11817ecc0100f835ccb3
JameJaga <59597475+JameJaga@users.noreply.github.com>
2020-02-03 18:22:46 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ async def on_member_join(member):
     channel = client.get_channel(673782210771550229)
     embed = discord.Embed(title="Joined",description = f'{str(member)}が参加。',color=discord.Colour.from_rgb(0, 255, 255))
     await channel.send(embed=embed)
-    role = discord.utils.get(message.guild.roles, name='NotCertified')
+    role = discord.utils.get(guild.roles, name='NotCertified')
     await member.add_roles(role)
 async def on_member_remove(member):
     channel = client.get_channel(673782210771550229)
