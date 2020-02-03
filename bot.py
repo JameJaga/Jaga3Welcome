14417575d46df72907bf7160434d97e5ef1eeb74
JameJaga <59597475+JameJaga@users.noreply.github.com>
2020-02-03 17:56:05 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,10 +9,21 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    channel = client.get_channel(673798279552565268)
+    await channel.purge()
+    await channel.send('**ジャガの部屋へようこそ！**\nわいわい雑談・ゲームしたりしましょう！:smile:\n招待リンクです→→ https://discord.gg/uGUKFPb')
 
 @client.event
 async def on_member_join(member):
-    channel = client.get_guild(662153006787199046)
+    #入退室ログ
+    channel = client.get_channel(673782210771550229)
+    embed = discord.Embed(title="Joined",description = f'{str(member)}が参加。',color=discord.Colour.from_rgb(0, 255, 255))
+    await message.channel.send(embed=embed)
     role = discord.utils.get(message.guild.roles, name='NotCertified')
     await member.add_roles(role)
+    channel = 
+async def on_member_remove(member):
+    channel = client.get_channel(673782210771550229)
+    embed = discord.Embed(title="Joined",description = f'{str(member)}が退出。',color=discord.Colour.from_rgb(255, 0, 0))
+    await message.channel.send(embed=embed)
 client.run(TOKEN)
