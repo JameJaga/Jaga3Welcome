04c8f59d778dc75b29b728d3457f6d1f98fe6ab5
JameJaga <59597475+JameJaga@users.noreply.github.com>
2020-02-04 17:44:33 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,5 +44,6 @@ async def on_raw_reaction_add(payload):
         channel = client.get_channel(674169297001775114)
         role = discord.utils.find(lambda r: r.name == '082', member.guild.roles)  
         await member.add_roles(role)
-        await channel.send(f'{str(member)}がジャガの部屋に来たよ！よろしく！ :smile:。') 
+        embed = discord.Embed(title="Joined",description = f'{str(member)}がジャガの部屋に来たよ！よろしく！ :smile:',color=discord.Colour.from_rgb(0, 255, 255))
+    await channel.send(embed=embed)
 client.run(TOKEN)
